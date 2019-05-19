0ac67f1d361752fbbdded4cbade98405cfa272e3
Amit Keren <amitkeren9@gmail.com>
2019-05-19 21:13:40 +0300

    Initial commit

---
+++ Dino.py
@@ -0,0 +1,127 @@
+import pygame
+import time
+import random
+from pygame.locals import *
+
+pygame.init()
+pygame.display.init()
+width, height = 1280, 720
+icon_surf = pygame.image.load("GameData/Sprites/dino0000.png")
+pygame.display.set_icon(icon_surf)
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Dinosaur Game')
+screen.fill((255, 255, 255))
+dinopos = [100, 500]
+keys = [False, False, False]
+# Loading sprites
+dino1 = pygame.image.load("GameData/Sprites/dinorun0000.png")
+dino2 = pygame.image.load("GameData/Sprites/dinorun0001.png")
+dinoduck0 = pygame.image.load("GameData/Sprites/dinoduck0000.png")
+dinoduck1 = pygame.image.load("GameData/Sprites/dinoduck0001.png")
+dinojump = pygame.image.load("GameData/Sprites/dinoJump0000.png")
+berd0 = pygame.image.load("GameData/Sprites/berd.png")
+berd1 = pygame.image.load("GameData/Sprites/berd2.png")
+cactussmall = pygame.image.load("GameData/Sprites/cactusSmall0000.png")
+cactusbig = pygame.image.load("GameData/Sprites/cactusBig0000.png")
+cactushord = pygame.image.load("GameData/Sprites/cactusSmallMany0000.png")
+dinoicon = pygame.image.load("GameData/Sprites/dino0000.png")
+dinodead = pygame.image.load("GameData/Sprites/dinoDead0000.png")
+texture = dino1
+running = True
+jumping = False
+duck = False
+
+
+def updatescreen():
+    """
+Used to refresh the screen after some changes, Fills the screen with the color, draws the line, blits the entities, and
+then flips (updates) the screen
+    :rtype: None
+    """
+    screen.fill(0)
+    pygame.draw.line(screen, (83, 83, 83), (0, 612), (1280, 612))
+    screen.blit(texture, dinopos)
+    pygame.display.flip()
+
+
+while running:
+    if not jumping and not duck:
+        texture = dino1
+        updatescreen()
+        texture = dino2
+        updatescreen()
+    if duck:
+        texture = dinoduck0
+        updatescreen()
+        texture = dinoduck1
+        updatescreen()
+    updatescreen()
+    for event in pygame.event.get():
+        # Check if X is pressed
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
+
+        # Check for event KEYDOWN
+
+        if event.type == pygame.KEYDOWN:
+            # Full screen key - F11
+            if event.key == K_F11:
+                keys[0] = True
+            # Jump key(s) - Space and K_up
+            if event.key == K_UP or event.key == K_SPACE:
+                keys[1] = True
+            # Duck key - K_DOWN
+            if event.key == K_DOWN:
+                keys[2] = True
+
+        # Check for EVENT KEYUP
+        if event.type == pygame.KEYUP:
+            # Full screen key - F11
+            if event.key == K_F11:
+                keys[0] = False
+            # Jump key(s) - Space and K_up
+            if event.key == K_UP or event.key == K_SPACE:
+                keys[1] = False
+            # Duck key - K_DOWN
+            if event.key == K_DOWN:
+                keys[2] = False
+
+        if keys[0]:
+            # print(pygame.display.get_wm_info())
+            # isFull = False
+            # if not isFull:
+            pygame.display.set_mode((width, height), pygame.FULLSCREEN, pygame.NOFRAME)
+            # print(pygame.display.get_wm_info())
+                # isFull = True
+            # elif isFull:
+                # pygame.display.set_mode((width, height))
+                # isFull = False
+            print('Toggled Full screen')
+        if keys[1]:
+            jumping = True
+            print('Jump')
+            texture = dinojump
+            for i in range(60):
+                dinopos[1] -= 2.5
+                # screen.fill(0)
+                # screen.blit(dino1, dinopos)
+                # pygame.display.flip()
+                updatescreen()
+            time.sleep(0.1)
+            for i in range(60):
+                dinopos[1] += 2.5
+                # screen.fill(0)     Remove later - used instead of updatescreen()
+                # screen.blit(dino1, dinopos)
+                # pygame.display.flip()
+                updatescreen()
+            jumping = False
+            # Continue later - Jump animation - 30 FPS (Frames per second)
+        if keys[2]:
+            print('Duck')
+            duck = True
+            dinopos[1] = 544
+            # updatescreen()
+        if not keys[2]:
+            duck = False
+            dinopos[1] = 500

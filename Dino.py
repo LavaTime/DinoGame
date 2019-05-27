25353fbfb54d09aad629182875d59f893da2580b
yonkad214 <yonkad214@gmail.com>
2019-05-27 15:42:50 +0300

    Update Dino.py
    tried collisions added lose def
    added score to screen

--- Dino.py
+++ Dino.py
@@ -19,6 +19,7 @@ keys = [False, False, False]
 
 # Loading game files
 
+FONT = pygame.font.SysFont("monospace", 16)
 DINO1 = pygame.image.load("GameData/Sprites/dinorun0000.png")
 DINO2 = pygame.image.load("GameData/Sprites/dinorun0001.png")
 DINODUCK0 = pygame.image.load("GameData/Sprites/dinoduck0000.png")
@@ -41,6 +42,13 @@ pygame.mixer.music.load(JUMPSFX)
 
 # Values
 
+losetext = FONT.render("You Lose", 1, (100, 100, 100))
+cactusbig_y = 508
+cactushord_y = 508
+cactussmall_y = 508
+bird_low_y = 500
+bird_mid_y = 422
+bird_high_y = 370
 obsPos = []
 obsList = []
 lastObs = 40
@@ -76,9 +84,14 @@ then flips (updates) the screen
     for x in range(len(obsList)):
         screen.blit(obsList[x], obsPos[x])
     screen.blit(dinotexture, dinopos)
+    screen.blit(scoretext, (1150, 20))
     pygame.display.flip()
 
-
+def lose():
+    screen.fill((255, 255, 255))
+    screen.blit(losetext, (WIDTH/2, HEIGHT/2))
+    pygame.time.delay(2000)
+    running = False
 '''def cloudspawn():
     global lastcloud
     if lastcloud >= mintimeclouds:
@@ -110,39 +123,40 @@ def obsSpawn():
     global score
     global lastObs
     # print(score, lastObs)
-    if (score - lastObs) > 40:
+    if (score - lastObs) > score/15:
         num = random.randint(0, 100)
         if num <= 25:
             obsList.append(CACTUSSMALL)
-            obsPos.append([WIDTH, 508])
+            obsPos.append([WIDTH, cactussmall_y])
             lastObs = score
         elif 25 < num <= 45:
             obsList.append(CACTUSBIG)
-            obsPos.append([WIDTH, 508])
+            obsPos.append([WIDTH, cactusbig_y])
             lastObs = score
         elif 45 < num <= 60:
             obsList.append(CACTUSHORD)
-            obsPos.append([WIDTH, 508])
+            obsPos.append([WIDTH, cactushord_y])
             lastObs = score
         elif 60 < num <= 75 and score > 250: # high
             obsList.append(BIRD0)
-            obsPos.append([WIDTH, 370])
+            obsPos.append([WIDTH, bird_high_y])
             lastObs = score
             print(60, 75)
         elif 75 < num <= 88 and score > 250: # low
             obsList.append(BIRD0)
-            obsPos.append([WIDTH, 500])
+            obsPos.append([WIDTH, bird_low_y])
             lastObs = score
             print(75, 88)
         elif 88 < num <= 100 and score > 250:
             # middle
             obsList.append(BIRD0)
-            obsPos.append([WIDTH, 422])
+            obsPos.append([WIDTH, bird_mid_y])
             lastObs = score
             print(88, 100)
 
 
 while running:
+    scoretext = FONT.render("Score " + str(score), 1, (100, 100, 100))
     # score += 1
 
     #print(int(score))
@@ -166,12 +180,31 @@ while running:
 
     # moves obstacles forward and deletes them off screen
     for x in range(len(obsPos)):
-        if obsPos[x][0] == 0 :
+        if obsList[x] == BIRD0 and obsPos[x][1] == bird_low_y:
+            if(obsPos[x][0] - 96 < dinopos[0] < obsPos[x][0] + 92 and dinopos[1]+112 < bird_low_y):
+                lose()
+        elif obsList[x] == BIRD0 and obsPos[x][1] == bird_mid_y:
+            if obsPos[x][0] - 96 < dinopos[0] < obsPos[x][0] + 92 and dinopos[1]+112 < bird_mid_y :
+                lose()
+        elif obsList[x] == BIRD0 and obsPos[x][1] == bird_high_y:
+            if obsPos[x][0] - 96 < dinopos[0] < obsPos[x][0] + 92 and bird_high_y + 80 < dinopos[1]+112 < bird_high_y :
+                lose()
+        elif obsList[x] == CACTUSBIG:
+            if(obsPos[x][0] - 60 < dinopos[0] < obsPos[x][0] + 92 and dinopos[1]+112 < cactusbig_y) :
+                lose()
+        elif obsList[x] == CACTUSHORD:
+            if (obsPos[x][0] - 120 < dinopos[0] < obsPos[x][0] + 92 and dinopos[1]+112 < cactushord_y) :
+                lose()
+        elif obsList[x] == CACTUSSMALL:
+            if (obsPos[x][0] - 40 < dinopos[0] < obsPos[x][0] + 92 and dinopos[1]+112 < cactussmall_y) :
+                lose()
+        # moves obs forward and deletes them
+        if obsPos[x][0] < 5:
             obsPos.pop(x)
             obsList.pop(x)
         elif obsPos[x][0] > -20:
-            obsPos[x][0] -= 1
-            # obsPos.insert(x, [obsPos[x][0] - 1, obsPos[x][1]])
+            obsPos[x][0] -= score/15
+
 
 
     updatescreen()
